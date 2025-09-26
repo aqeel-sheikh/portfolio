67710f1b8d8a4da1f95c427bd8d563bd9c980b5d
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-09-26 23:10:27 +0530

    feat(contact-form): Enhance form submission with result display and API integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,42 @@ function hightlightCurrentSection(){
         sideNavItems[sectionIndex].classList.add("active")
     }
 }
+const form = document.getElementById('my-form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.style.display = "block"
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Thanks for reaching out! I'll get back to you soon.";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
