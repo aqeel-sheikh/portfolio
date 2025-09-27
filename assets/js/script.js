dc0e437e03163838bd8ec86265dce77b425ca127
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-09-27 19:11:03 +0530

    feat(hero-section): Revamp hero title with dynamic text and enhance styling
    - Replaced custom css typewriter effect with typed.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,72 +14,83 @@ const swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-const sections = document.querySelectorAll(".section")
+const sections = document.querySelectorAll(".section");
 let sectionIndex;
 
 window.addEventListener("scroll", trackSectionOnScroll);
-window.addEventListener("load", trackSectionOnScroll)
+window.addEventListener("load", trackSectionOnScroll);
 
-function trackSectionOnScroll () {
-    for(let i = 0; i < sections.length; i++){
-        if(sections[i].getBoundingClientRect().top < window.innerHeight / 1.8 && sections[i].getBoundingClientRect().bottom > window.innerHeight / 1.6){
-            sectionIndex = i
-            hightlightCurrentSection()
-        }
+function trackSectionOnScroll() {
+  for (let i = 0; i < sections.length; i++) {
+    if (
+      sections[i].getBoundingClientRect().top < window.innerHeight / 1.8 &&
+      sections[i].getBoundingClientRect().bottom > window.innerHeight / 1.6
+    ) {
+      sectionIndex = i;
+      hightlightCurrentSection();
     }
+  }
 }
-function hightlightCurrentSection(){
-    const sideNavItems = document.querySelectorAll(".side-nav a");
-    for(let i = 0; i < sideNavItems.length; i++){
-        if(sideNavItems[i].classList.contains("active")){
-            sideNavItems[i].classList.remove("active")
-        }
+function hightlightCurrentSection() {
+  const sideNavItems = document.querySelectorAll(".side-nav a");
+  for (let i = 0; i < sideNavItems.length; i++) {
+    if (sideNavItems[i].classList.contains("active")) {
+      sideNavItems[i].classList.remove("active");
     }
-    if(!sideNavItems[sectionIndex].classList.contains("active")){
-        sideNavItems[sectionIndex].classList.add("active")
-    }
+  }
+  if (!sideNavItems[sectionIndex].classList.contains("active")) {
+    sideNavItems[sectionIndex].classList.add("active");
+  }
 }
-const form = document.getElementById('my-form');
-const result = document.getElementById('result');
+const form = document.getElementById("my-form");
+const result = document.getElementById("result");
 
-form.addEventListener('submit', function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   const formData = new FormData(form);
   const object = Object.fromEntries(formData);
   const json = JSON.stringify(object);
-  result.style.display = "block"
-  result.innerHTML = "Please wait..."
+  result.style.display = "block";
+  result.innerHTML = "Please wait...";
 
-    fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-        .then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = "Thanks for reaching out! I'll get back to you soon.";
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(function() {
-            form.reset();
-            setTimeout(() => {
-                result.style.display = "none";
-            }, 3000);
-        });
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML =
+          "Thanks for reaching out! I'll get back to you soon.";
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 3000);
+    });
+});
+// Typed.js
+const typed = new Typed(".dynamic-title", {
+  strings: ["Crafting modern, user-friendly web experiences.", "Turning ideas into clean, responsive code.", "Building sleek interfaces with attention to detail."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
 });
