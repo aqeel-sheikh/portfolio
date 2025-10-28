ae763796b7ed546561575532a23efa7022090a3d
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-10-29 02:09:44 +0530

    fix: Remove swiper.js because of appearing bugs in carousel - Remove min-height on sections for better ui/ux in large screens - Add React and Tailwind in about section - Restyle project cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,3 @@
-const swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
 
 const sections = document.querySelectorAll(".section");
 let sectionIndex;
